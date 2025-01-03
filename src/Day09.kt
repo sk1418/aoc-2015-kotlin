21109b25dd4ff4f36fb2be1ba35b44c0fe75ad42
Kent <kent.yuan@Gmail.com>
2025-01-04 00:34:56 +0100

    Day 09 ✅

--- src/Day09.kt
+++ src/Day09.kt
@@ -8,26 +8,34 @@ fun main() {
     val testInput = readTestInput(today)
 
     val re = """(\S+) to (\S+) = (\d+)""".toRegex()
-    fun part1(input: List<String>): Int {
-        val distances = mutableListOf<Int>()
-        val places = mutableSetOf<String>()
-        input.forEach {
-            re.find(it).also { m ->
-                places.add(m!!.groups[1]!!.value)
-                places.add(m.groups[2]!!.value)
-                distances.add(m.groups[3]!!.value.toInt())
+
+    fun parseInput(input: List<String>): MutableNotNullMap<Set<String>, Int> {
+        return buildMap {
+            input.forEach {
+                re.find(it).also { m ->
+                    put(setOf(m!!.groups[1]!!.value, m.groups[2]!!.value), m.groups[3]!!.value.toInt())
+                }
             }
+        }.toMutableNotNullMap()
+    }
+
+    fun part1(input: List<String>): Int {
+        val theMap = parseInput(input)
+        return permutations(theMap.keys.flatten().distinct()).minOf{ route ->
+            route.windowed(2).sumOf { pair -> theMap[pair.toSet()] }
         }
-        return distances.sorted().take(places.size-1).sum()
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val theMap = parseInput(input)
+        return permutations(theMap.keys.flatten().distinct()).maxOf{ route ->
+            route.windowed(2).sumOf { pair -> theMap[pair.toSet()] }
+        }
     }
 
     chkTestInput(Part1, testInput, 605) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0L) { part2(it) }
+    chkTestInput(Part2, testInput, 982) { part2(it) }
     solve(Part2, input) { part2(it) }
 }
