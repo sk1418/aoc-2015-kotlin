98ba4be9bd63576d72c50e56cade128f30c97e89
Kent <kent.yuan@Gmail.com>
2025-01-04 18:31:34 +0100

    Day 19, 20 ✅

--- src/Day19.kt
+++ src/Day19.kt
@@ -6,18 +6,47 @@ fun main() {
 
     val input = readInput(today)
     val testInput = readTestInput(today)
-
-    fun part1(input: List<String>): Long {
-        return 0
+    fun parseInput(input: List<String>): Pair<String, List<Pair<String, String>>> {
+        val replacements = input.takeWhile { it.isNotBlank() }.map { it.split(" => ").let { it[0] to it[1] } }
+        val line = input.last()
+        return line to replacements
     }
 
-    fun part2(input: List<String>): Long {
-        return 0
+    fun part1(input: List<String>): Int {
+        val (txt, replacements) = parseInput(input)
+        return replacements.map { (from, to) ->
+            buildList {
+                var scanIdx = 0
+                while (true) {
+                    val start = txt.indexOf(from, startIndex = scanIdx)
+                    if (start < 0) break
+                    txt.let { it.substring(0, start) + to + it.substring(start + from.length) }
+                        .also { add(it) }
+                    scanIdx = start + from.length
+                }
+            }
+        }.flatten().distinct().size
     }
 
-    chkTestInput(Part1, testInput, 0L) { part1(it) }
+    fun part2(input: List<String>): Int {
+        val (txt, replacements) = parseInput(input)
+        var target = txt
+        var times = 0
+        while (target != "e") {
+            replacements.forEach { (from, to) ->
+                if (to in target) {
+                    target = target.replaceFirst(to, from)
+                    times++
+                }
+            }
+        }
+
+        return times
+    }
+
+    chkTestInput(Part1, testInput, 7) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0L) { part2(it) }
+    chkTestInput(Part2, testInput, 6) { part2(it) }
     solve(Part2, input) { part2(it) }
 }
--- src/Day20.kt
+++ src/Day20.kt
@@ -7,17 +7,39 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Long {
-        return 0
+    fun part1(input: List<String>): Int {
+        val threshold = input.single().toInt()
+        val houses = MutableList(threshold / 10 + 1) { 0 }
+        val elfs = List(threshold / 10 + 1) { it }.drop(1)
+        elfs.forEach {
+            var seq = it
+            while (seq <= houses.lastIndex) {
+                houses[seq] += (10 * it)
+                seq += it
+            }
+        }
+        return houses.indexOfFirst { it > threshold }
     }
 
-    fun part2(input: List<String>): Long {
-        return 0
+    fun part2(input: List<String>): Int {
+        val threshold = input.single().toInt()
+        val houses = MutableList(threshold / 10 + 1) { 0 }
+        val elfs = List(threshold / 10 + 1) { it }.drop(1)
+        elfs.forEach {
+            var seq = it
+            var times = 0
+            while (seq <= houses.lastIndex && times <= 50) {
+                houses[seq] += (11 * it)
+                seq += it
+                times++
+            }
+        }
+        return houses.indexOfFirst { it > threshold }
     }
 
-    chkTestInput(Part1, testInput, 0L) { part1(it) }
+    chkTestInput(Part1, testInput, 6) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0L) { part2(it) }
+//    chkTestInput(Part2, testInput, 0L) { part2(it) }
     solve(Part2, input) { part2(it) }
 }
