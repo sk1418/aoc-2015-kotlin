387eff53b2d789a9ff9b0cf486b34f70044b45a2
Kent <kent.yuan@Gmail.com>
2025-01-04 23:40:47 +0100

    Day 24 ✅

--- src/Day24.kt
+++ src/Day24.kt
@@ -7,17 +7,27 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Long {
-        return 0
-    }
+    fun part1(input: List<String>): Long = Day24(input).solve(3)
 
-    fun part2(input: List<String>): Long {
-        return 0
-    }
+    fun part2(input: List<String>): Long = Day24(input).solve(4)
 
-    chkTestInput(Part1, testInput, 0L) { part1(it) }
+    chkTestInput(Part1, testInput, 99) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0L) { part2(it) }
+    chkTestInput(Part2, testInput, 44) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+private data class Day24(val input: List<String>) {
+    val nums = input.map { it.toInt() }
+    fun solve(groupSize: Int): Long {
+        val targetSum = nums.sum() / groupSize
+        for (i in 2..nums.lastIndex - 2) {
+            val list = nums.combinations(i).filter { it.sum() == targetSum }
+            if (list.isNotEmpty()) {
+                return list.minOf { it.fold(initial = 1L) { acc, next -> acc * next.toLong() } }
+            }
+        }
+        return -1
+    }
 }
